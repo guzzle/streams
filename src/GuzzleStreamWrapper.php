1bc1aa803afe4f23ae0f794d6a832998cc7055e8
Chris Leppanen <chris.leppanen@gmail.com>
2014-04-01 21:03:55 -0700

    Remove PhpStream.

--- src/GuzzleStreamWrapper.php
+++ src/GuzzleStreamWrapper.php
@@ -3,19 +3,66 @@
 namespace GuzzleHttp\Stream;
 
 /**
- * PHP stream implementation
+ * PHP stream wrapper implementation.
  */
 class GuzzleStreamWrapper
 {
+    /** @var StreamInterface[] */
+    private static $streams = array();
 
-    public static $streams = array();
+    /** @var StreamInterface */
+    protected $stream;
+
+    /**
+     * Returns a resource representing the stream.
+     *
+     * @param StreamInterface $stream The stream to get a resource for
+     *
+     * @return resource
+     */
+    public static function getResource(StreamInterface $stream)
+    {
+        return fopen('guzzle://' . static::registerStream($stream), 'r+');
+    }
+
+    /**
+     * Registers a stream to be used as a PHP stream wrapper.
+     *
+     * @param StreamInterface $stream A stream
+     *
+     * @return string                 The hash of the stream
+     */
+    public static function registerStream(StreamInterface $stream)
+    {
+        if (!in_array('guzzle', stream_get_wrappers())) {
+            stream_wrapper_register('guzzle', get_called_class());
+        }
+        $hash = spl_object_hash($stream);
+        self::$streams[$hash] = $stream;
+
+        return $hash;
+    }
+
+    /**
+     * Unregisters a stream.
+     *
+     * @param StreamInterface $stream A stream
+     */
+    public static function unregisterStream(StreamInterface $stream)
+    {
+        unset(self::$streams[spl_object_hash($stream)]);
+    }
 
     public function stream_open($path, $mode, $options, &$opened_path)
     {
         list(, $hash) = explode('://', $path);
-        $this->stream = static::$streams[$hash];
 
-        return true;
+        if (isset(self::$streams[$hash])) {
+            $this->stream = self::$streams[$hash];
+            return true;
+        }
+
+        return false;
     }
 
     public function stream_read($count)
@@ -25,25 +72,30 @@ class GuzzleStreamWrapper
 
     public function stream_write($data)
     {
-        return $this->stream->write($data);
+        return (int) $this->stream->write($data);
     }
 
-    function stream_tell()
+    public function stream_close()
+    {
+        static::unregisterStream($this->stream);
+    }
+
+    public function stream_tell()
     {
         return $this->stream->tell();
     }
 
-    function stream_eof()
+    public function stream_eof()
     {
         return $this->stream->eof();
     }
 
-    function stream_seek($offset, $whence)
+    public function stream_seek($offset, $whence)
     {
         return $this->stream->seek($offset, $whence);
     }
 
-    function stream_metadata($path, $option, $var)
+    public function stream_metadata($path, $option, $var)
     {
         return false;
     }
