1a00b163e414b72b0155e35e81f3b116d6a1d07c
Michael Dowling <mtdowling@gmail.com>
2014-08-20 14:24:03 -0700

    Flushing the decorated stream when seeking and closing

--- src/StreamDecoratorTrait.php
+++ src/StreamDecoratorTrait.php
@@ -63,8 +63,14 @@ trait StreamDecoratorTrait
         return $result === $this->stream ? $this : $result;
     }
 
+    /**
+     * Calls flush() and closes the underlying stream.
+     */
     public function close()
     {
+        // Allow the decorated stream to flush any buffered content on close.
+        $this->flush();
+        // Close the decorated stream.
         $this->stream->close();
     }
 
@@ -110,8 +116,18 @@ trait StreamDecoratorTrait
         return $this->stream->isSeekable();
     }
 
+    /**
+     * Calls flush() and seeks to the specified position in the stream.
+     *
+     * {@inheritdoc}
+     */
     public function seek($offset, $whence = SEEK_SET)
     {
+        // Flush the stream before seeking to allow decorators to flush their
+        // state before losing their position in the stream.
+        // see: https://github.com/php/php-src/blob/8b66d64b2343bc4fd8aeabb690024edb850a0155/main/streams/streams.c#L1312
+        $this->flush();
+
         return $this->stream->seek($offset, $whence);
     }
 
